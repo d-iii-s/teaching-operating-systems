3471ad9e6ac0006627ec8253897d70be2b10b717
Petr Tuma <petr.tuma@d3s.mff.cuni.cz>
2021-09-27 14:39:16 +0200

    Make function name consistent with self study instructions

--- src/assembly-program-image/commented-allocation-function/alloc.c
+++ src/assembly-program-image/commented-allocation-function/alloc.c
@@ -1,12 +1,13 @@
 #include <errno.h>
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 
-void *safe_malloc (size_t size) {
-    void *result = malloc (size);
-    if (result == NULL) {
+void *checked_malloc (size_t size) {
+    void *addr = malloc (size);
+    if (addr == NULL) {
         printf ("Failed to allocate %zi bytes.\n", size);
         exit (ENOMEM);
     }
-    return (result);
+    return (addr);
 }
