23fcaf4b1d868361bcbb062d925393330f539b52
Petr Tuma <petr.tuma@d3s.mff.cuni.cz>
2021-09-27 14:55:11 +0200

    Few more examples

---
+++ src/assembly-program-image/simple-memory-addresses/print-main-global.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int i = 1234;
+
+int main (void) {
+    printf ("main: %p i: %p\n", main, &i);
+    return (0);
+}
---
+++ src/assembly-program-image/simple-memory-addresses/print-main-local.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void) {
+    int i = 1234;
+    printf ("i: %p\n", &i);
+    return (0);
+}
