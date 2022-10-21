e03e07a242c6fd92e3e00c41942a3dea4a66c3db
Petr Tuma <petr.tuma@d3s.mff.cuni.cz>
2022-10-21 15:02:23 +0200

    Fixed one too many bug in heap leak

--- src/various-memory-allocation/memory-corruption-patterns/heap-leak.c
+++ src/various-memory-allocation/memory-corruption-patterns/heap-leak.c
@@ -7,9 +7,6 @@ int main (void) {
   // Allocate an array on heap.
   int *array = (int *) malloc (sizeof (int) * SIZE);
 
-  // Write just past the end of the array.
-  array [SIZE] = 0;
-
   // Do not release the array.
   return (0);
 }
